8f37406a95a7bd40edc7552dec4166b8f06965e9
waltertse980 <152850544+waltertse980@users.noreply.github.com>
2026-03-22 20:16:45 +0800

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -19,7 +19,9 @@ async function setupMemoryLane() {
 
     const revealDate = new Date("2026-03-25T18:30:00+08:00");
     const now = new Date();
-    const isTestAdmin = (guestData.uid === 'admin_003' || guestData.uid === 'guest_000');
+    const isTestAdmin = (guestData.uid === 'admin_003' 
+    //    || guestData.uid === 'guest_000'
+    );
 
     // 3. Time Logic
     if (now < revealDate && !isTestAdmin) {
